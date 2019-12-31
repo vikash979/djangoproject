c34bd40f14a5fd72575c1de01945c5bf4ea30a94
vikash979 <vikash.kumar979@gmail.com>
2020-01-01 04:23:09 +0530

    myapi apps custom user add now succesully done 1-01-2020 new with more column added (New AbstractBaseUser)

--- myapi/drfapps/forms.py
+++ myapi/drfapps/forms.py
@@ -102,7 +102,7 @@ class RegisterForm(forms.ModelForm):
         password1 = self.cleaned_data.get("password1")
         password2 = self.cleaned_data.get("password2")
         if password1 and password2 and password1 != password2:
-            raise forms.ValidationError("Passwords don't match")
+            raise forms.ValidationError("Passwords don't matchs")
         return password2
 
 
@@ -121,7 +121,7 @@ class UserAdminCreationForm(forms.ModelForm):
         password1 = self.cleaned_data.get("password1")
         password2 = self.cleaned_data.get("password2")
         if password1 and password2 and password1 != password2:
-            raise forms.ValidationError("Passwords don't match")
+            raise forms.ValidationError("Passwords don't matchee")
         return password2
 
     def save(self, commit=True):
